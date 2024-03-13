4e654a150181cf6ed1aab1a88c6ca55238a67423
dani <danifromecuador@gmail.com>
2024-03-13 13:05:54 -0500

    Log a message in the console every x minutes

--- src/pages/Alarm.jsx
+++ src/pages/Alarm.jsx
@@ -1,32 +1,42 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import '../styles/Alarm.css'
 
 export const Alarm = () => {
-  const [alarmState, setAlarmState] = useState("turn on")
-  const [message, setMessage] = useState("alarm is off")
+  const [alarmState, setAlarmState] = useState("turn on alarm")
+
+
   const handleOnOffAlarmBtn = () => {
-    if (alarmState === "turn on") {
-      setAlarmState("turn off")
-      setMessage("alarm is active")
+    if (alarmState === "turn on alarm") {
+      setAlarmState("turn off alarm")
+      console.log("alarm is on")
     }
     else {
-      setAlarmState("turn on")
-      setMessage("alarm is off")
+      setAlarmState("turn on alarm")
+      console.log("alarm is off")
     }
   }
 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const currentDate = new Date();
+      const minutes = currentDate.getMinutes();
+      const seconds = currentDate.getSeconds()
+      if (alarmState==="turn off alarm" && minutes % 1 === 0 && seconds % 5 === 0) {
+        console.log(`Alarm sound at ${minutes}:${seconds}`);
+      }
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [alarmState]);
+
   return (
     <div className="alarm">
-      <p className='message'>
-        {message}
-      </p>
       <button
         className={`on-off-alarm-btn ${alarmState}`}
         onClick={handleOnOffAlarmBtn}
       >
         {alarmState}
       </button>
-
     </div>
   )
 }
