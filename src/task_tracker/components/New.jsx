d31c40e66509d8d5fb9c99c74045273d8a28d51a
dani <danifromecuador@gmail.com>
2024-03-15 19:07:55 -0500

    Manage taks input

--- src/task_tracker/components/New.jsx
+++ src/task_tracker/components/New.jsx
@@ -1,7 +1,53 @@
+import { useState } from "react"
+import PropTypes from 'prop-types'
+
 export const New = () => {
+  const [input, setInput] = useState("")
+  const [completedTask, setCompletedTask] = useState([])
+
+  const handleInput = (e) => setInput(e.target.value)
+
+  const handleCompletedTaskAddBtn = () => {
+    setCompletedTask([...completedTask], input)
+    console.log(completedTask);
+    setInput("")
+  }
+
+  const handleEnterKey = (e) => {
+    if (e.key === "Enter") handleCompletedTaskAddBtn()
+  }
+
+
+
+
+
+
+
+
   return (
     <div className="new">
-      add a done task
+      <div className="current-block">
+        18:00 - 18:30
+      </div>
+      <input
+        type="text"
+        placeholder="enter completed task"
+        value={input}
+        onChange={(e) => handleInput(e)}
+        onKeyDown={handleEnterKey}
+
+      />
+      <button
+        type="button"
+        className="add-completed-task-btn"
+        onClick={handleCompletedTaskAddBtn}
+      >
+        Add
+      </button>
     </div>
   )
+}
+
+New.propTypes = {
+  CompletedTask: PropTypes.oneOf([PropTypes.number, PropTypes.string]).isRequired,
 }
