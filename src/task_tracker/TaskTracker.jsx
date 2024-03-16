92fe3cee3c6c88da1198b5eb9649d7f7e33040eb
dani <danifromecuador@gmail.com>
2024-03-15 22:42:17 -0500

    Delete testing code

--- src/task_tracker/TaskTracker.jsx
+++ src/task_tracker/TaskTracker.jsx
@@ -1,5 +1,4 @@
-import { useState, useEffect } from 'react'
-
+import { useState } from 'react'
 import { Counter } from './components/Counter.jsx'
 import { Tasks } from './components/Tasks.jsx'
 import { New } from './components/New.jsx'
@@ -7,19 +6,13 @@ import { New } from './components/New.jsx'
 export const TaskTracker = () => {
   const [array, setArray] = useState([])
 
-  const getData = (data) => {
-    setArray([...data])
-  }
-
-  useEffect(()=>{
-    console.log(array);
-  }, [array])
+  const getData = (data) => setArray([...data])
 
   return (
     <div className="task-tracker">
       <Counter />
-      <Tasks Array={array}/>
-      <New Data={getData}/>
+      <Tasks Array={array} />
+      <New Data={getData} />
     </div>
   )
 }
