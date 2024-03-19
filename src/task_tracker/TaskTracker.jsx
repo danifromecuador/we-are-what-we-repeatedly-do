1fbba48f110c356d3da887fb572cb6f72c463b14
dani <danifromecuador@gmail.com>
2024-03-19 16:37:34 -0500

    Save in local storage

--- src/task_tracker/TaskTracker.jsx
+++ src/task_tracker/TaskTracker.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Counter } from './components/Counter.jsx'
 import { Tasks } from './components/Tasks.jsx'
 import { New } from './components/New.jsx'
 
 export const TaskTracker = () => {
-  const [array, setArray] = useState([])
+  const [array, setArray] = useState(JSON.parse(localStorage.getItem("completed-tasks-list")) || [])
   const [waitMessage, setWaitMessage] = useState("")
 
-  const getData = (task) => {
-    console.log(`task: ${task.time_range} ${task.text}`);
+  const getData = (task) => { // add to the array a new completed task
     if (array.length > 0) {
       if (array[array.length - 1].time_range !== task.time_range) setArray([...array, task])
       else { // show a wait message for 3 seconds
@@ -21,11 +20,19 @@ export const TaskTracker = () => {
     else setArray([...array, task]) // this will execute just if the array is empty
   }
 
+  const deleteArray = () => setArray([])
+
+  // saves on local storage when the array changes
+  useEffect(() => {
+    localStorage.setItem("completed-tasks-list", JSON.stringify(array))
+  }, [array])
+
   return (
     <div className="task-tracker">
-      <Counter />
+      <Counter Array={array} />
       <Tasks Array={array} />
-      {waitMessage}
+      <button onClick={deleteArray}>Delete Today's Progress</button>
+      <div className="wait-message">{waitMessage}</div>
       <New Data={getData} />
     </div>
   )
